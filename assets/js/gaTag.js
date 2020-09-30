0d4b5cbe472db87a59759d1bb82cd6fa391e40c2
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2020-09-30 15:43:34 +0100

    wip: add no js fallback content

--- assets/js/gaTag.js
+++ assets/js/gaTag.js
@@ -1,12 +1,30 @@
 'use strict';
 
-function run() {
+function hideFallbackContent() {
+  var fallbackContent = document.getElementsByClassName('js-disabled');
+  for (var i = 0; i < fallbackContent.length; i++) {
+    fallbackContent[i].style.display = 'none';
+  }
+}
+
+function showInteractiveContent() {
+  var interactiveContent = document.getElementsByClassName('js-enabled');
+  for (var i = 0; i < interactiveContent.length; i++) {
+    interactiveContent[i].style.display = 'block';
+  }
+}
+
+function initialiseCookieBanner() {
   var banner = document.getElementById("global-cookie-message");
   if (banner !== null) {
+    hideFallbackContent();
+    showInteractiveContent();
     banner.style.display = 'block';
   }
 }
 
-run();
+initialiseCookieBanner();
 
-module.exports = {};
+module.exports = {
+  initialiseCookieBanner
+};
--- test/gaTag.test.js
+++ test/gaTag.test.js
@@ -13,8 +13,26 @@ describe('ga-tag', () => {
     document.body.appendChild(cookieBanner);
   });
 
-  test('it should append cookie banner for test setup', () => {
-    expect(document.body.firstChild).toEqual(cookieBanner);
-  })
+  describe('initialiseCookieBanner', () => {
+
+    beforeAll(() => {
+      gaTag.initialiseCookieBanner();
+    });
+
+    test('it should hide fallback content if JS is enabled', () => {
+      const fallbackContent = document.getElementsByClassName('js-disabled');
+      for (let element of fallbackContent) {
+        expect(element.style.display).toEqual('none');
+      }
+    });
+
+    test('it should show interactive content if JS is enabled', () => {
+      const interactiveContent = document.getElementsByClassName('js-enabled');
+      for (let element of interactiveContent) {
+        expect(element.style.display).toEqual('block');
+      }
+    });
+
+  });
 
 });
