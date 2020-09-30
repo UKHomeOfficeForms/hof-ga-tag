2294454877daace359702e421d09d1b9447f4510
alexfranklin <alex.franklin@digital.homeoffice.gov.uk>
2020-09-30 16:49:00 +0100

    wip: hide cookie banner on pages not within HOF apps

--- assets/js/gaTag.js
+++ assets/js/gaTag.js
@@ -15,11 +15,18 @@ function showInteractiveContent() {
 }
 
 function initialiseCookieBanner() {
-  var banner = document.getElementById("global-cookie-message");
-  if (banner !== null) {
+  var bannerContainer = document.getElementById("global-cookie-message"); // the default cookie message container from hof-govuk-template
+  var cookieBanner    = document.getElementById("cookie-banner");         // the cookie banner that will fill replace the container's default content if using this package
+
+  if (bannerContainer !== null && cookieBanner !== null) {
     hideFallbackContent();
     showInteractiveContent();
-    banner.style.display = 'block';
+    bannerContainer.style.display = 'block';
+  }
+
+  // hide default cookie message on pages not in HOF sub apps
+  if (bannerContainer !== null && cookieBanner === null) {
+    bannerContainer.style.display = 'none';
   }
 }
 
--- test/gaTag.test.js
+++ test/gaTag.test.js
@@ -4,22 +4,36 @@ const path = require('path');
 const gaTag = require('../assets/js/gaTag');
 
 describe('ga-tag', () => {
+  let bannerContainer;
   let cookieBanner;
 
-  beforeAll(() => {
-    cookieBanner = document.createElement('div');
-    cookieBanner.id = 'global-cookie-message';
-    cookieBanner.innerHTML = fs.readFileSync(path.join(__dirname, '../views/partials/cookie-banner.html'), 'utf8');
-    document.body.appendChild(cookieBanner);
+  beforeEach(() => {
+    bannerContainer = document.createElement('div');
+    bannerContainer.id = 'global-cookie-message';
+    bannerContainer.innerHTML = fs.readFileSync(path.join(__dirname, '../views/partials/cookie-banner.html'), 'utf8');
+    cookieBanner = bannerContainer.firstChild;
+    document.body.appendChild(bannerContainer);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(bannerContainer);
   });
 
   describe('initialiseCookieBanner', () => {
 
-    beforeAll(() => {
+    test('it should show banner container if container and banner are both present', () => {
       gaTag.initialiseCookieBanner();
+      expect(bannerContainer.style.display).toEqual('block');
+    });
+
+    test('it should hide banner container if present without cookie banner (i.e. on pages outside HOF sub apps)', () => {
+      bannerContainer.removeChild(cookieBanner);
+      gaTag.initialiseCookieBanner();
+      expect(bannerContainer.style.display).toEqual('none');
     });
 
     test('it should hide fallback content if JS is enabled', () => {
+      gaTag.initialiseCookieBanner();
       const fallbackContent = document.getElementsByClassName('js-disabled');
       for (let element of fallbackContent) {
         expect(element.style.display).toEqual('none');
@@ -27,6 +41,7 @@ describe('ga-tag', () => {
     });
 
     test('it should show interactive content if JS is enabled', () => {
+      gaTag.initialiseCookieBanner();
       const interactiveContent = document.getElementsByClassName('js-enabled');
       for (let element of interactiveContent) {
         expect(element.style.display).toEqual('block');
